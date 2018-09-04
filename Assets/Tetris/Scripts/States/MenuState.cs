c6f8b5c351c04788a2de78b6cb2f5682c65c0be8
Syy <shirakawa.yy@gmail.com>
2018-09-04 21:26:43 +0900

    Add empty MenuState

---
+++ Assets/Tetris/Scripts/States/MenuState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Syy.State
+{
+    public class MenuState : StateBase
+    {
+        public override void OnActivate()
+        {
+            
+        }
+
+        public override void OnDeactivate()
+        {
+            // throw new System.NotImplementedException();
+        }
+
+        public override void OnUpdate()
+        {
+            // throw new System.NotImplementedException();
+        }
+    }
+}
