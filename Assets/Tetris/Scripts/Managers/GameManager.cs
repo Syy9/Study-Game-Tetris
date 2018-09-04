c88be4f6cf469371396520994b7eb55e8444fb30
Syy <shirakawa.yy@gmail.com>
2018-09-04 21:18:27 +0900

    Add GameManager

---
+++ Assets/Tetris/Scripts/Managers/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Syy.Manager {
+
+    public class GameManager : MonoBehaviour
+    {
+        public bool isGameActive;
+        void Awake()
+        {
+            isGameActive = false;
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+
+}
