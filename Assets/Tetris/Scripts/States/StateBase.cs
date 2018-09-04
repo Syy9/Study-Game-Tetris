90aa50e93508ff08ad6502c35bbecff04ae7c695
Syy <shirakawa.yy@gmail.com>
2018-09-04 21:19:03 +0900

    Add StateBase

---
+++ Assets/Tetris/Scripts/States/StateBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Syy.State
+{
+    public abstract class StateBase : MonoBehaviour
+    {
+        public abstract void OnActivate();
+        public abstract void OnDeactivate();
+        public abstract void OnUpdate();
+        public override string ToString()
+        {
+            return this.GetType().ToString();
+        }
+    }
+}
