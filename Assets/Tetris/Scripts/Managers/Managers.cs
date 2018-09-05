0f27489f2fbf02c5c8f5528dd50fabcb8ed14726
Syy <shirakawa.yy@gmail.com>
2018-09-05 22:36:36 +0900

    Add Managers class

---
+++ Assets/Tetris/Scripts/Managers/Managers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Syy.Manager
+{
+    public class Managers : MonoBehaviour
+    {
+        static GameManager Game;
+        void Awake()
+        {
+            Game = GetComponent<GameManager>();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
