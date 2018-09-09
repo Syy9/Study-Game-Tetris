87381f5e031ec1ac91728d9985b53b9868d5f8d0
Syy <shirakawa.yy@gmail.com>
2018-09-09 22:59:58 +0900

    Add ColorManager

---
+++ Assets/Tetris/Scripts/Managers/ColorManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Syy.Manager
+{
+    public enum ColorTheme
+    {
+        PASTEL,
+        GRAM,
+    }
+
+    public class ColorManager : MonoBehaviour
+    {
+
+        public ColorTheme activeTheme;
+        public Color[] themePack_Pastel;
+        public Color[] themePack_Gram;
+
+        public Color TurnRandomColorFromTheme()
+        {
+            int rand = Random.RandomRange(0,6);
+            Color temp;
+            switch (activeTheme)
+            {
+                case ColorTheme.PASTEL:
+                    temp = themePack_Pastel[rand];
+                    break;
+                case ColorTheme.GRAM:
+                    temp = themePack_Gram[rand];
+                    break;
+                default:
+                    temp = Color.black;
+                    break;
+            }
+
+            return temp;
+        }
+    }
+}
